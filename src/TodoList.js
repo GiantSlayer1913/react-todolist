3ec83077d52c55508de21dce5b58b1789c56423a
GiantSlayer1913 <michaeljaredfisch@gmail.com>
2018-11-15 14:11:40 -0500

    App working properly without react-flip-move added

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import TodoItems from "./TodoItems";
+import "./TodoList.css";
 
 class TodoList extends Component {
   constructor(props) {
@@ -9,6 +11,7 @@ class TodoList extends Component {
     };
 
     this.addItem = this.addItem.bind(this);
+    this.deleteItem = this.deleteItem.bind(this);
   }
 
   addItem(event) {
@@ -31,6 +34,19 @@ class TodoList extends Component {
 
     event.preventDefault();
   }
+
+  deleteItem(key) {
+    console.log("key in deleteItem:" + key);
+    console.log("Items at delete: " + this.state.items);
+    var filteredItems = this.state.items.filter(function (item) {
+      return (item.key !== key)
+    });
+
+    this.setState({
+      items: filteredItems
+    });
+  }
+
   render() {
     return (
       <div className="todoListMain">
@@ -42,6 +58,8 @@ class TodoList extends Component {
             <button type="submit">add</button>
           </form>
         </div>
+        <TodoItems entries={this.state.items}
+                   delete={this.deleteItem}/>
       </div>
     );
   }
