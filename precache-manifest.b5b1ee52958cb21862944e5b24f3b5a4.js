3448dacc7625dafb57a4658b820eef3e69bd447a
GiantSlayer1913 <michaeljaredfisch@gmail.com>
2018-11-16 12:41:11 -0500

    Updates

---
+++ precache-manifest.b5b1ee52958cb21862944e5b24f3b5a4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "fe622147818fbc58e51b",
+    "url": "/static/js/main.fe622147.chunk.js"
+  },
+  {
+    "revision": "2fa7c0d4973f34ea5972",
+    "url": "/static/js/1.2fa7c0d4.chunk.js"
+  },
+  {
+    "revision": "fe622147818fbc58e51b",
+    "url": "/static/css/main.babe08b8.chunk.css"
+  },
+  {
+    "revision": "e7e3aa1a1a5bccd1c93834d2c98b1654",
+    "url": "/index.html"
+  }
+];
